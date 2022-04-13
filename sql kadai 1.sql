cd89321f41a3cb81f6638392df7db1ef7e1db8b5
iraha kei <22iraha_kei@axiz.co.jp>
2022-04-13 19:08:08 +0900

    Fist commit

---
+++ sql kadai 1.sql
@@ -0,0 +1,13 @@
+ CREATE TABLE major
+ ( major_id INT PRIMARY KEY
+ , major_name VARCHAR(50)
+ );
+
+ CREATE TABLE student
+ (student_id INT PRIMARY KEY,
+ student_name VARCHAR(50),
+ grade INT,
+ hometown VARCHAR(50),
+major_id INT NOT NULL REFERENCES major(major_id)
+ );
+ 
